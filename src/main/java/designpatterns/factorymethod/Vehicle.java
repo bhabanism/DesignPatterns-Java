b287df7962395e04eb22a9f4a78041197dff13e3
Bhabani <bhabanism@outlook.com>
2017-02-22 22:39:26 +0530

    #3 Added Example of Factory Method Pattern using vehicles example

---
+++ src/main/java/designpatterns/factorymethod/Vehicle.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.factorymethod;
+
+/**
+ *
+ * @author Mishra
+ */
+public abstract class Vehicle {
+    int wheels; //can be any other objects - family of object - composition
+    boolean body;
+    boolean handle;
+    boolean stirring;
+    boolean engine;
+    boolean paddle;
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setBody(boolean body) {
+        this.body = body;
+    }
+
+    public void setHandle(boolean handle) {
+        this.handle = handle;
+    }
+
+    public void setStirring(boolean stirring) {
+        this.stirring = stirring;
+    }
+
+    public void setEngine(boolean engine) {
+        this.engine = engine;
+    }
+
+    public void setPaddle(boolean paddle) {
+        this.paddle = paddle;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public boolean isBody() {
+        return body;
+    }
+
+    public boolean isHandle() {
+        return handle;
+    }
+
+    public boolean isStirring() {
+        return stirring;
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public boolean isPaddle() {
+        return paddle;
+    }
+}
